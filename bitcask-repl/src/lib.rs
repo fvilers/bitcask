b1d1eda9dffbd1937ac7628d5466ae237789237b
Fabian Vilers <fabian.vilers@dev-one.com>
2023-02-19 19:53:10 +0100

    Add argument parsing for directory name

--- bitcask-repl/src/lib.rs
+++ bitcask-repl/src/lib.rs
@@ -2,11 +2,27 @@ use std::error::Error;
 
 use bitcask_lib::prelude::*;
 
-pub fn run() -> Result<(), Box<dyn Error>> {
+pub struct RunConfig {
+    pub directory_name: String,
+}
+
+impl RunConfig {
+    pub fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 2 {
+            return Err("Usage: bitcask-repl <directory_name>");
+        }
+
+        let directory_name = args[1].to_owned();
+
+        Ok(Self { directory_name })
+    }
+}
+
+pub fn run(config: RunConfig) -> Result<(), Box<dyn Error>> {
     let datastore = OpenOptions::new()
         .write(true)
         .sync(true)
-        .open("c:\\temp\\datastore")?;
+        .open(config.directory_name)?;
 
     println!("{:?}", datastore);
 
--- bitcask-repl/src/main.rs
+++ bitcask-repl/src/main.rs
@@ -1,10 +1,17 @@
-use std::process;
+use std::{env, process};
+
+use bitcask_repl::RunConfig;
 
 fn main() {
     // This crate will be a REPL and accept as a startup argument the directory name of the Bitcask datastore.
     // As for now, there will be no interaction with the user
+    let args: Vec<String> = env::args().collect();
+    let config = RunConfig::build(&args).unwrap_or_else(|error| {
+        println!("{}", error);
+        process::exit(1);
+    });
 
-    if let Err(error) = bitcask_repl::run() {
+    if let Err(error) = bitcask_repl::run(config) {
         eprintln!("{}", error);
         process::exit(1);
     }
